8d14011daaf16bf13832ca3ac9f4240fc441ca8b
wanghengg <wh997yyk@163.com>
2024-09-06 21:59:24 +0800

    [feat] 单例模式

--- src/main/java/com/example/singleton/LazySingleton.java
+++ src/main/java/com/example/singleton/LazySingleton.java
@@ -1,6 +1,12 @@
 package com.example.singleton;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 public class LazySingleton {
+    private static final Logger log = Logger.getGlobal();
     private LazySingleton() {}
     private volatile static LazySingleton instance;
     // 通过 synchronized 关键词解决并发问题，但是锁定范围比较大，导致性能降低
@@ -17,12 +23,23 @@ public class LazySingleton {
         if (instance == null) {
             // 使用 synchronized 关键词只锁定实例化过程，减小锁定范围
             synchronized (LazySingleton.class) {
+                log.info("第一次进来");
                 // 第二重判断
                 if (instance == null) {
+                    log.info("第二次进来");
                     instance = new LazySingleton();
                 }
             }
         }
         return instance;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(LazySingleton::getInstance);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(100, TimeUnit.MINUTES);
     }
 }
