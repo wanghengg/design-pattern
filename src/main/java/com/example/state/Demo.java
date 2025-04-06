8533aec0907e82645ff888b01c74a6f3c469e1b5
wangheng <wh9697yyk@gmail.com>
2025-04-06 12:29:41 +0800

    状态模式

---
+++ src/main/java/com/example/state/Demo.java
@@ -0,0 +1,13 @@
+package com.example.state;
+
+public class Demo {
+    public static void main(String[] args) {
+        VendingMachine vendingMachine = new VendingMachine(1);
+        vendingMachine.insertCoin();
+        vendingMachine.ejectCoin();
+        vendingMachine.dispense();
+        vendingMachine.insertCoin();
+        vendingMachine.dispense();
+        vendingMachine.insertCoin();
+    }
+}
---
+++ src/main/java/com/example/state/VendingMachineState.java
@@ -0,0 +1,16 @@
+package com.example.state;
+
+/**
+ * 状态模式（State Pattern）是一种行为型设计模式，它允许一个对象在其内部状态改变时改变它的行为，
+ * 对象看起来似乎修改了它的类。状态模式将状态的判断逻辑和状态对应的行为封装在不同的状态类中，
+ * 使得状态的切换和行为的执行更加清晰和易于维护，避免了大量的条件判断语句（如 if-else 或 switch）。
+ */
+
+public interface VendingMachineState {
+    // 投币
+    void insertCoin();
+    // 退币
+    void ejectCoin();
+    // 购买商品
+    void dispense();
+}
