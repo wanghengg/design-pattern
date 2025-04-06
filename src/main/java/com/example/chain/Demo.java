33bcf62ba87dbf33cb570f164afd84ec7cbdc028
wangheng <wh9697yyk@gmail.com>
2025-04-06 13:05:28 +0800

    责任链模式

---
+++ src/main/java/com/example/chain/Demo.java
@@ -0,0 +1,14 @@
+package com.example.chain;
+
+public class Demo {
+    public static void main(String[] args) {
+        GeneralManager generalManager = new GeneralManager();
+        DepartmentManager departmentManager = new DepartmentManager();
+        departmentManager.setNextApprover(generalManager);
+        TeamLeader teamLeader = new TeamLeader();
+        teamLeader.setNextApprover(departmentManager);
+        teamLeader.processRequest(1);
+        teamLeader.processRequest(5);
+        teamLeader.processRequest(15);
+    }
+}
